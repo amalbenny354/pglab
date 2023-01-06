fd7db9cad356019bf158a01437c948d98e0e4a87
amal <116335467+amalbenny354@users.noreply.github.com>
2023-01-06 10:29:43 +0530

    Add files via upload

---
+++ character replaced.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+main_string = input("Type a string : ")
+char = main_string[0]
+main_string = main_string.replace(char,'$')
+print(char+main_string[1:])
+
+
+# In[ ]:
+
+
+
+
