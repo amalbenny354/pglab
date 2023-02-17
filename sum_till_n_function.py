c59b9e6ccc4e863792ba1c7263348c2c3dae74ce
amal <116335467+amalbenny354@users.noreply.github.com>
2023-02-17 10:58:31 +0530

    Add files via upload

---
+++ sum_till_n_function.py
@@ -0,0 +1,7 @@
+def sum(a):
+    s=0
+    for i in range(1,a+1):
+        s=s+i
+    print(s)
+a=int(input("enter a:"))
+sum(a)                                                                                 
